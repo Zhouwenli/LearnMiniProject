957d5f772694c3a011c6c84465946182767da503
Zhouwenli <1021092219@qq.com>
2020-02-05 19:40:35 +0800

    组件父子之间相互

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,11 +5,15 @@ Page({
    * 页面的初始数据
    */
   data: {
+    titles:['流行','新款','精选'],
     bannarImg: ['/assets/bannar/1.jpg', '/assets/bannar/2.jpg', '/assets/bannar/3.jpg'],
     indicatorDots: true,
     vertical: false,
     autoplay: true,
     interval: 2000,
     duration: 500
+  },
+  handleItemClick(event){
+    console.log(event)
   }
 })
