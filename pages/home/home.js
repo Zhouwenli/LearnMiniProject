c4de51e1ebb82dc68127c6bb1df3f561ca68cfb9
Zhouwenli <1021092219@qq.com>
2020-02-05 17:10:03 +0800

    轮播图

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,18 +5,11 @@ Page({
    * 页面的初始数据
    */
   data: {
-    name:'zhouwenli',
-    age:'25',
-    students:[
-      { id: '101', name: 'zhou1', age: '30' },
-      { id: '102', name: 'zhou2', age: '30' },
-      { id: '103', name: 'zhou3', age: '30' }
-    ],
-    counter:0
-  },
-  handleClick(){
-    this.setData({
-      counter: this.data.counter +1
-    })
+    bannarImg: ['/assets/bannar/1.jpg', '/assets/bannar/2.jpg', '/assets/bannar/3.jpg'],
+    indicatorDots: true,
+    vertical: false,
+    autoplay: true,
+    interval: 2000,
+    duration: 500
   }
 })
